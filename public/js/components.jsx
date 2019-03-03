a00de1825c5ec88426a65a27f9a2d35ab8807877
Dylan Wright <sealsurlaw@gmail.com>
2019-03-03 03:43:59 -0800

    Names

--- public/js/components.jsx
+++ public/js/components.jsx
@@ -79,7 +79,7 @@ class Moves extends React.Component {
 class Stream extends React.Component {
     render() {
         return (
-            <iframe src="http://10.143.236.177:8000/index.html" height="280" width="340" className="center border-none">
+            <iframe src="http://10.143.236.177:8000/index.html" height="280" width="340" className="center border-none stream">
             </iframe>
         );
     }
@@ -220,7 +220,7 @@ class Chat extends React.Component {
     render() {
         return (
             <div id="chat" className="container-fluid">
-                {this.props.name}<br />
+                <div className="full-width">{this.props.name}</div>
                 <input type="text" className="full-width" value={this.state.value} onChange={this.handleChange} />
             </div>
         );
