81da10a3738657bcaa7507a53eece5794e5a71b0
Dylan Wright <sealsurlaw@gmail.com>
2019-03-03 02:49:04 -0800

    Formatting

--- public/js/components.jsx
+++ public/js/components.jsx
@@ -65,7 +65,7 @@ class Moves extends React.Component {
 class Stream extends React.Component {
     render() {
         return (
-            <iframe src="http://10.143.236.177:8000/index.html" height="280" width="340" className="center">
+            <iframe src="http://10.143.236.177:8000/index.html" height="280" width="340" className="center border-none">
             </iframe>
         );
     }
