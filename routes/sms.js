87e2af857d19d86358452d5c60f5845b6004de83
Dylan Wright <sealsurlaw@gmail.com>
2019-03-03 04:49:04 -0800

    Names

--- routes/sms.js
+++ routes/sms.js
@@ -1,17 +1,12 @@
-const express = require('express');
+var express = require('express');
+var router = express.Router();
 var accountSid = 'AC20000fc501ef3991d171e0dbbe8fd1df'; // Your Account SID from www.twilio.com/console
 var authToken = '877dd76a608187d7d6526748c1c0e464';   // Your Auth Token from www.twilio.com/console
 var twilio = require('twilio');
 var client = new twilio(accountSid, authToken);
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
-const bodyParser = require('body-parser');
-
-const app = express();
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.post('/sms', (req, res) => {
+router.post('/sms', (req, res) => {
     const twiml = new MessagingResponse();
 
     if (req.body.Body.toUpperCase() == 'FORWARD') {
@@ -29,3 +24,5 @@ app.post('/sms', (req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/xml' });
     res.end(twiml.toString());
 });
+
+module.exports = router;
