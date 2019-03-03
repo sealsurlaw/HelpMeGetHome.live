9a908c31e21a8e682dc7153b64f2039847620311
Dylan Wright <sealsurlaw@gmail.com>
2019-03-03 03:10:42 -0800

    Formatting

--- routes/name.js
+++ routes/name.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../db');
+var names = require('../public/js/names.json');
 
 router.get('/', function (req, res, next) {
     db.any(`SELECT * FROM controls;`)
