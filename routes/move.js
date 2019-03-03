79e0468580d5142f145a4a810ad64fe6e0d2d845
Dylan Wright <sealsurlaw@gmail.com>
2019-03-02 23:48:25 -0800

    Finished vote route

--- routes/move.js
+++ routes/move.js
@@ -5,8 +5,10 @@ var db = require('../db');
 router.get('/', function (req, res, next) {
     db.any(`SELECT * FROM controls;`)
         .then(data => {
-            console.log(data);
-            res.send(data[0])
+            db.any(`UPDATE controls SET "left" = 0, "right" = 0, "forward" = 0, "backward" = 0, "moves" = "moves" + 1;`)
+                .then(_ => {
+                    res.send(data[0]);
+                })
         })
         .catch(err => {
             console.log(err);
